509a279cef7553d0081d2dbdc89f2b947ae2d66f
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2023-06-23 07:30:26 +0300

    fin assets
    added more lang icons

--- assets.py
+++ assets.py
@@ -1,18 +1,24 @@
 import pygame
 
-BACKGROUND = pygame.image.load('Assets/Background.png')
+BACKGROUND = pygame.image.load('Assets/background.png')
 BODY = pygame.image.load('Assets/PythonBody.png')
 HEAD_UP = pygame.image.load('Assets/PythonHeadUp.png')
 HEAD_DOWN = pygame.image.load('Assets/PythonHeadDown.png')
 HEAD_RIGHT = pygame.image.load('Assets/PythonHeadRight.png')
 HEAD_LEFT = pygame.image.load('Assets/PythonHeadLeft.png')
+C = pygame.image.load('Assets/c.png')
 CPLUS = pygame.image.load('Assets/cplusplus.png')
 CSHARP = pygame.image.load('Assets/csharp.png')
 JS = pygame.image.load('Assets/js.png')
 JAVA = pygame.image.load('Assets/java.png')
+R = pygame.image.load('Assets/r.png')
 RUBY = pygame.image.load('Assets/ruby.png')
 GOLANG = pygame.image.load('Assets/golang.png')
 PHP = pygame.image.load('Assets/php.png')
 RUST = pygame.image.load('Assets/rust.png')
 SWIFT = pygame.image.load('Assets/swift.png')
-FOOD_FOR_PYTHON = [CPLUS, CSHARP, JS, JAVA, RUST, RUBY, GOLANG, PHP, SWIFT]
+SCALA = pygame.image.load('Assets/scala.png')
+KOTLIN = pygame.image.load('Assets/kotlin.png')
+HASKELL = pygame.image.load('Assets/haskell.png')
+
+FOOD_FOR_PYTHON = [CPLUS, CSHARP, JS, JAVA, RUST, RUBY, GOLANG, PHP, SWIFT, SCALA, HASKELL, KOTLIN, R, C]
